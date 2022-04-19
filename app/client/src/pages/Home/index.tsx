06a6a10328fc095155a879b6a0db4c3b9a1e8b18
felipenevess <felipe012neves@gmail.com>
2022-04-19 14:35:34 -0300

    feat: add page Home

---
+++ app/client/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Menu } from '../../components';
+
+function Home() {
+  return (
+    <div>
+      <p>Home</p>
+      <Menu />
+    </div>
+  );
+}
+
+export default Home;
