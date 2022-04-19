202c058ea7b5a1ee64292b3701560c3a2dde4525
felipenevess <felipe012neves@gmail.com>
2022-04-18 21:14:48 -0300

    feat: config react-router-dom

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <p>Felipe Neves</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={ <Home /> }  />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
