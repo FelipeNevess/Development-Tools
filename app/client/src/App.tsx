a719730437a1b1f50a9a6f030ce91be40ce2e9f6
felipenevess <felipe012neves@gmail.com>
2022-04-19 14:33:10 -0300

    chore: removing routes from app.jsx

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home';
+import Routes from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"  element={ <Home /> }  />
-      </Routes>
-    </BrowserRouter>
+    <Routes />
   );
 }
 
