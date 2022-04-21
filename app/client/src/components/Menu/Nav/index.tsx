2db3e3979f322b6d39394931783d8e9f527fffc8
felipenevess <felipe012neves@gmail.com>
2022-04-20 21:29:23 -0300

    feat: config menu component

--- app/client/src/components/Menu/Nav/index.tsx
+++ app/client/src/components/Menu/Nav/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import { Icons, AiFillHome } from './Utils/icons';
+import { Icons, AiFillHome } from './utils/icons';
+import { DarkModeContext } from '../../../context/DarkModeProvider';
 
 function Nav() {
+  const { active } = useContext(DarkModeContext).context;
+
   return (
     <nav>
       <ul>
         <li className="l10">
           <span className="span-home">Home</span>
-          { <AiFillHome /> }
+          { <AiFillHome className={`cvg10-${active} svg-dark-${active}` } /> }
         </li>
         <div>
           {
@@ -17,7 +20,7 @@ function Nav() {
                 return (
                   <li className={`l${i}`} key={i} >
                     <span>{ icon.title }</span>
-                    { <icon.inc /> }
+                    { <icon.inc className={`cvg${i}-${active} svg-dark-${active}`} /> }
                   </li>
                 );
               }
