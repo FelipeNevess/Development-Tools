e8e28d8ce3d683124374067645346912406e1be6
felipenevess <felipe012neves@gmail.com>
2022-04-21 22:57:44 -0300

    feat: export default component

--- app/client/src/components/index.tsx
+++ app/client/src/components/index.tsx
@@ -1,2 +1,5 @@
 export { default as Menu } from './Menu';
 export { default as DarkMode } from './DarkMode';
+export { default as PageStructure } from './PageStructure';
+export { default as NavOptions } from './NavOptions';
+export { default as Data } from './_FrontEndInfos';
