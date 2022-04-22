2a637a0781b2c062d92f375688ca9cd0bef53585
felipenevess <felipe012neves@gmail.com>
2022-04-21 23:05:06 -0300

    feat: add windows install

---
+++ app/client/src/components/_FrontEndInfos/Install/Windows/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function WindowsInstall() {
+  return (
+    <main>
+      <article>
+        <p>
+          O Windows 10 é a versão mais recente do sistema operacional da Microsoft e representa
+          uma série de mudanças relevantes na forma como o SO é produzido, distribuído e mantido
+          pela desenvolvedora norte-americana. Ao contrário de edições anteriores,
+          o Windows 10 é vendido como um serviço: em vez de um Windows 11, por exemplo, os
+          usuários recebem grandes atualizações, que promovem a adição de novos recursos e
+          tecnologias de forma gratuita e horizontal, já que todo mundo passa a ter acesso a
+          essas novidades.
+        </p>
+      </article>
+    </main>
+  );
+}
+
+export default WindowsInstall;
