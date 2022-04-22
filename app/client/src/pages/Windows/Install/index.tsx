02494ab41e4d8115aa84e2b70f10e415754b1f5e
felipenevess <felipe012neves@gmail.com>
2022-04-21 23:09:07 -0300

    feat: create windows pages

---
+++ app/client/src/pages/Windows/Install/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { FaWindows } from 'react-icons/fa';
+import { PageStructure, NavOptions, Data } from '../../../components';
+
+function WindowsInstall() {
+  return (
+    <PageStructure
+      icon={ <FaWindows />}
+      title='Windows 10'
+      filter={
+        <NavOptions
+          installation
+          settings
+          pageName='windows'
+        />
+      }
+      filterLinux={ Data[0].installations }
+    />
+  );
+}
+
+export default WindowsInstall;
