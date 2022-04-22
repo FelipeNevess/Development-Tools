346d6f2ff511624708fc2911c3cc3b99c9807e65
felipenevess <felipe012neves@gmail.com>
2022-04-21 23:02:54 -0300

    chore: useMemo context

--- app/client/src/context/DarkModeProvider/index.tsx
+++ app/client/src/context/DarkModeProvider/index.tsx
@@ -1,11 +1,11 @@
-import { createContext, useState } from 'react';
+import React, { createContext, useMemo, useState } from 'react';
 
 import { IContextTools } from './interfaces';
 import { DEFAULT_VALUES } from './DefaultValues';
 
 export const DarkModeContext = createContext<IContextTools>(DEFAULT_VALUES);
 
-export const DarkModeProvider = ({ children}) => {
+export function DarkModeProvider({ children}) {
   const [active, setActive] = useState<boolean>(false);
 
   const context = {
@@ -13,7 +13,9 @@ export const DarkModeProvider = ({ children}) => {
     setActive,
   };
 
-  return <DarkModeContext.Provider value={{ context }}>
+  const obj = useMemo(() => ({context}), [active]);
+
+  return <DarkModeContext.Provider value={obj}>
       {children}
     </DarkModeContext.Provider>;
 }
