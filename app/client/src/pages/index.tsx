ed7ad5b39811651d4481b63ef0ff56a02164eca9
felipenevess <felipe012neves@gmail.com>
2022-04-25 11:41:36 -0300

    refactor: index page

--- app/client/src/pages/index.tsx
+++ app/client/src/pages/index.tsx
@@ -1,4 +1,2 @@
 export { default as Home } from './Home';
-export { default as WindowsAbout } from './Windows/About';
-export { default as WindowsInstall } from './Windows/Install';
-export { default as WindowsSettings } from './Windows/Settings';
+export { default as Windows } from './Windows';
